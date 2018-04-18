b9ee5d4b365b1dbf661972a580f870707596a892
mpatience <36686711+mpatience@users.noreply.github.com>
2018-04-17 18:33:54 -0700

    Update cone.js

--- Assignment3/cone.js
+++ Assignment3/cone.js
@@ -13,13 +13,14 @@ function init() {
 
     gl.clearColor( 1.0, 0.0, 1.0, 1.0 );
 
-    cone = new Cone(gl, 4)
+    cone = new Cone(gl, 4);
     render();
     
 }
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
+    cone.render();
 }
 
 window.onload = init;
