20b19aae9725e482a6f0f11c74074647a98c4e85
mpatience <36686711+mpatience@users.noreply.github.com>
2018-04-24 18:33:07 -0700

    Update cone.js

--- Assignment3/cone.js
+++ Assignment3/cone.js
@@ -13,7 +13,7 @@ function init() {
 
     gl.clearColor( 1.0, 0.0, 1.0, 1.0 );
 
-    cone = new Cone(gl, 10);
+    cone = new Cone(gl, 10, "Cone-vertex-shader", "Cone-fragment-shader");
     render();
     
 }
